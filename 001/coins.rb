b96e403a1f7807b9be7282093b0d75f348a514db
Allen Wipf <allenwipf@msn.com>
2017-01-24 11:42:53 -0600

    my first commit

---
+++ 001/coins.rb
@@ -0,0 +1,21 @@
+def coins(amount)
+
+   qCount = ((amount / 0.25).floor)
+   amount = (amount % 0.25)
+
+   dCount = ((amount / 0.10).floor)
+   amount = (amount % 0.10)
+
+   nCount = ((amount / 0.05).floor)
+   amount = (amount % 0.05)
+
+   pCount = (amount / 0.01).round
+
+   print "You have #{qCount} quarters, #{dCount} dimes, #{nCount} nickes, and #{pCount} pennies!"
+
+   return amount
+   
+end
+
+
+coins(1.46)
