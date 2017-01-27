bdfece4760d2d26d10c760b2765eb31a6649534e
Allen Wipf <allenwipf@msn.com>
2017-01-27 01:00:40 -0600

    after finishing questions.rb

---
+++ 003/questions.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+require 'pry'
+first_arg, *the_rest = ARGV
+
+#binding pry
+
+# conditions. checks which function to run
+
+if first_arg == "add" then #if add is passed then add questions passed
+
+	new_question = the_rest.join(' ')
+	lineNumber = 0
+
+elsif first_arg == "rm" then  #if rm is passed then removed line indicated
+
+	lineNumber = the_rest.join(' ').to_i
+
+elsif first_arg.nil? #if no arguement is passed then print each questions
+
+		questions = File.open("questions.txt", "r")
+
+		questions.readlines.each_with_index do |eachLine, idx|
+
+		print eachLine
+
+ 		end
+
+	questions.close
+	lineNumber = 0
+
+end
+ 
+# if first_arg == "add" then add function
+
+def add(new_question)
+
+
+	questions = File.open("questions.txt", "a")
+
+	questions.puts new_question
+
+	questions.close
+
+end
+
+add(new_question)
+
+# if first_arg == "rm" then run removeLine function
+
+def removeLine(lineNumber)
+  
+
+  lineNumber = lineNumber - 1
+  newArray =  ""
+
+	questions = File.open("questions.txt", "r+")
+
+		questions.readlines.each_with_index do |eachLine, idx|
+
+			if idx != lineNumber then 
+			 
+			 newArray += eachLine
+
+			end
+ 	    end
+    questions.close
+
+ 	questions = File.open("questions.txt", "w")
+ 	questions.puts newArray
+
+	questions.close
+
+end
+
+removeLine(lineNumber)
+
+
+
+
+# BELOW IS NOT PART OF PROGRAM YET!!!
+#This function reads a specific line from a file and closes it
+def readLine(line)
+  
+  line = line - 1
+
+	questions = File.open("questions.txt", "r")
+
+		questions.readlines.each_with_index do |eachLine, idx|
+
+		print eachLine if idx == line
+
+ 		end
+
+	questions.close
+
+end
---
+++ 003/testing_argv.rb
@@ -0,0 +1,74 @@
+require 'pry'
+first_arg, *the_rest = ARGV
+
+#binding pry
+
+# conditions. checks which function to run
+
+if first_arg == "add" then #if add is passed then add questions passed
+
+	new_question = the_rest.join(' ')
+	lineNumber = 0
+
+elsif first_arg == "rm" then  #if rm is passed then removed line indicated
+
+	lineNumber = the_rest.join(' ').to_i
+
+elsif first_arg.nil? #if no arguement is passed then print each questions
+
+		questions = File.open("questions.txt", "r")
+
+		questions.readlines.each_with_index do |eachLine, idx|
+
+		print eachLine
+
+ 		end
+
+	questions.close
+	lineNumber = 0
+
+end
+ 
+# if first_arg == "add" then add function
+
+def add(new_question)
+
+
+	questions = File.open("questions.txt", "a")
+
+	questions.puts new_question
+
+	questions.close
+
+end
+
+add(new_question)
+
+# if first_arg == "rm" then run removeLine function
+
+def removeLine(lineNumber)
+  
+
+  lineNumber = lineNumber - 1
+  newArray =  ""
+
+	questions = File.open("questions.txt", "r+")
+
+		questions.readlines.each_with_index do |eachLine, idx|
+
+			if idx != lineNumber then 
+			 
+			 newArray += eachLine
+
+			end
+ 	    end
+    questions.close
+
+ 	questions = File.open("questions.txt", "w")
+ 	questions.puts newArray
+
+	questions.close
+
+end
+
+removeLine(lineNumber)
