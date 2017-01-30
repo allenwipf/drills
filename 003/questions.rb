061f2c8eda6bf94fb51a3f6d88d23c1487b4598a
Allen Wipf <allenwipf@msn.com>
2017-01-29 21:17:12 -0600

    pushing to submit...not sure if chances where made

--- 003/questions.rb
+++ 003/questions.rb
@@ -2,63 +2,32 @@
 require 'pry'
 first_arg, *the_rest = ARGV
 
-#binding pry
-
-# conditions. checks which function to run
-
-
-if first_arg == "add" then #if add is passed then add questions passed
-
-	new_question = the_rest.join(' ')
-	lineNumber = 0
-
-elsif first_arg == "rm" then  #if rm is passed then removed line indicated
-
-	lineNumber = the_rest.join(' ').to_i
-
-elsif first_arg.nil? #if no arguement is passed then print each questions
-
-	questions = File.open("/Users/Wipf/questions.txt", "r")
-
-	questions.readlines.each_with_index do |eachLine, idx|
-
-	print "#{idx +1}. #{eachLine}"
-	
-end
-
-	questions.close
-	lineNumber = 0
-
-end
  
-# if first_arg == "add" then add function
+# if the first argument == "add" then this code appends the second argument passed to the file
 
 def add(new_question)
 
-
 	questions = File.open("/Users/Wipf/questions.txt", "a")
-
 	questions.puts new_question
-
 	questions.close
 
 end
 
-add(new_question)
+# if first_arg == "rm" then this code will open the file and copy each line into a new array unless 
+# it's index matches the number passed by second_arg or is an empty line. That array is then, line 
+# by line printed to the same file after it's erased. 
 
-# if first_arg == "rm" then run removeLine function
-
-def removeLine(lineNumber)
+def removeLine(line_to_remove)
   
 
-	lineNumber = lineNumber - 1
+	line_to_remove = line_to_remove - 1
 	newArray =  ""
 
 	questions = File.open("/Users/Wipf/questions.txt", "r+")
 
 		questions.readlines.each_with_index do |eachLine, idx|
 
-			if idx != lineNumber && eachLine.delete("#{idx}\n") != "" #each line has an index and /n ; .delete("#{idx}\n")removes it
+			if idx != line_to_remove && eachLine.delete("#{idx}\n") != "" #each line has an index and /n ; .delete("#{idx}\n")removes it
 												   		 
 			 	newArray += eachLine
 
@@ -67,31 +36,40 @@ def removeLine(lineNumber)
 
     questions.close
 
- 	questions = File.open("questions.txt", "w")
+ 	questions = File.open("/Users/Wipf/questions.txt", "w")
  	questions.puts newArray
 
 	questions.close
 
 end
 
-removeLine(lineNumber)
-
-
-
-
-# BELOW IS NOT PART OF PROGRAM YET!!!
-#This function reads a specific line from a file and closes it
-def readLine(line)
-  
-  line = line - 1
-
-	questions = File.open("/Users/Wipf/questions.txt", "r")
-
+def print(should_print)
+	if file == "yes"
+		questions = File.open("/Users/Wipf/questions.txt", "r")
 		questions.readlines.each_with_index do |eachLine, idx|
+		print "#{idx +1}. #{eachLine}"
+	end
 
-		print eachLine if idx == line
+end
 
- 		end
+
+#If statements that determines which functions run based on first_arg value
+
+if first_arg == "add" then #if add is passed then add questions passed
+
+	new_question = the_rest.join(' ')
+	add(new_question)
+
+elsif first_arg == "rm" then  #if rm is passed then removed line indicated
+
+	line_to_remove = the_rest.join(' ').to_i
+	removeLine(line_to_remove)
+
+elsif first_arg.nil? #if no arguement is passed then print each questions
+
+	print("yes")
+	
+end
 
 	questions.close
 
