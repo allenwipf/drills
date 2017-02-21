1ab0411ef731f9d20145ad384965c466e9d24b43
Allen Wipf <allenwipf@msn.com>
2017-02-21 14:32:43 -0600

    finished

--- 022/public/scripts.js
+++ 022/public/scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function(){
 
-	movieArrayNum = 0
+	
     document.getElementsByTagName("input")[1].addEventListener("click", getInfo)
     document.getElementsByClassName("prev")[0].addEventListener("click", negVar)
     document.getElementsByClassName("next")[0].addEventListener("click", plusVar)   
@@ -9,6 +9,7 @@ window.addEventListener("load", function(){
 
 function getInfo(e){
 
+	movieArrayNum = 0
     textField = document.getElementsByTagName("input")[0].value
     var getRequest = new XMLHttpRequest();
     getRequest.open('GET', '/results?query=' + textField);
@@ -16,6 +17,9 @@ function getInfo(e){
     getRequest.onload = function() {
 
         rawData = JSON.parse(getRequest.responseText);
+
+        document.getElementsByClassName("nav")[0].style.display = "block"
+
         createHtml(rawData)
     };
     getRequest.send();
@@ -24,32 +28,37 @@ function getInfo(e){
 
 function plusVar(e){
 
-	if (movieArrayNum < rawData.Search.length){
+	if (movieArrayNum < rawData.Search.length -1){
 		movieArrayNum ++
-		// getInfo(e)
 		createHtml(rawData)
+	} else {
+
+		document.getElementsByClassName("next")[0].style.display = "none";
 	}
-	
+
+	document.getElementsByClassName("prev")[0].style.display = "inline-block";	
 }
 
 function negVar(e){
  
 	if (movieArrayNum > 0){
 		movieArrayNum --
-		// getInfo(e)
 		createHtml(rawData)
+	} else {
+
+		document.getElementsByClassName("prev")[0].style.display = "none";
 	}
+	document.getElementsByClassName("next")[0].style.display = "inline-block";	
 }
 
 function createHtml(rawData){
 
 	var movie = rawData.Search[movieArrayNum]
 
-	htmlString = "<strong>" + movie.Title + "</strong>" + "<br>" 
-	+ movie.Year + " " + movie.Type + "<br>"
-	+ "<img src=" + movie.Poster + ">"
-	+ "<br>" + "IMBD Id = " + movie.imdbID
+	htmlString = (movieArrayNum + 1) + " of " + (rawData.Search.length) + "<br>"
+	+ "<strong>" + movie.Title + "</strong>" + "<br>" 
+	+ "<img src=" + movie.Poster + "> </br>"
+	+ movie.Year + " " + movie.Type + "<br>" + "IMBD Id = " +  + " " + movie.imdbID
 
 	document.getElementsByClassName("movieInfo")[0].innerHTML = htmlString
-
 }
